3474a0f5546a66b7674925ec50eb2e348674f331
Elsa <ehazzim@gmail.com>
2024-06-08 21:27:14 +0100

    Setting up functions and main file

---
+++ main.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+
+// functions
+void start()
+{
+    std::cout << "You find yourself in a dungeon.\n" << std::endl;
+}
+
+// main body
+int main()
+{
+    start();
+
+    system("pause");
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+// main body
+int main()
+{
+    std::cout << "You find yourself in a dungeon." << std::endl;
+
+    return 0; // ensures successful execution
+
+    system("pause");
+}
